d5cd07f253fd4937921991dd9810fe3754afbbb5
Matej Šircelj <sircelj.m@gmail.com>
2016-12-07 07:40:49 +0100

    FIX absolute ARN

--- lambdas/webpack_single.config.js
+++ lambdas/webpack_single.config.js
@@ -20,44 +20,58 @@ var first_line = array[0];
 var lambda_fn_name = first_line.substring(3).trim();
  
 var AWS = require('aws-sdk');
+var iam = new AWS.IAM();
+
 AWS.config.loadFromPath(config.credentials_path);
 
-module.exports = {
-  entry: entry, 
-  output: {
-    path: path.join(__dirname, "./bin"),
-    library: "[name]",
-    libraryTarget: "commonjs2",
-    filename: "[name]/[name].js"
-  },
-  externals: {
-    "aws-sdk": "aws-sdk"
-  },
-  target: "node",
-
-  module: {
-    loaders: [{
-      test: /\.json$/,
-      loader: 'json'
-    }]
-  },
-  
-  plugins: [    
-    new AWSUploadPlugin({
-      aws_config: require("F:/iam/lbp.json"),
-      lambda_name: config.lambda_prefix+"_"+lambda_fn_name,
-      lambda_handler: dirname+".handler",
-      lambda_role: "arn:aws:iam::128137337832:role/optiprocure_auth",
-      lambda_runtime: "nodejs4.3", // java8, nodejs, nodejs4.3, python2.7
-      lambda_timeout: 10,
-      lambda_memory_size: 512,
 
 
-      asset: "[path].zip",
-      test: /\.html$|\.js$|\.xml$|\.rels$|/,
-      threshold: 10240,
-      minRatio: 0.8,
-      verbose: true
-    }),
-  ]
-};
+module.exports = new Promise(function(resolve, reject){  
+    iam.getRole({
+      RoleName: config.role_name
+    }, function(err, data) {
+      if (err) {
+        reject(new Error("ERROR iam.getRole"));
+      }else{
+        resolve({
+          entry: entry, 
+          output: {
+            path: path.join(__dirname, "./bin"),
+            library: "[name]",
+            libraryTarget: "commonjs2",
+            filename: "[name]/[name].js"
+          },
+          externals: {
+            "aws-sdk": "aws-sdk"
+          },
+          target: "node",
+
+          module: {
+            loaders: [{
+              test: /\.json$/,
+              loader: 'json'
+            }]
+          },
+          
+          plugins: [    
+            new AWSUploadPlugin({
+              aws_config: require("F:/iam/lbp.json"),
+              lambda_name: config.lambda_prefix+"_"+lambda_fn_name,
+              lambda_handler: dirname+".handler",
+              lambda_role: data.Role.Arn,
+              lambda_runtime: "nodejs4.3", // java8, nodejs, nodejs4.3, python2.7
+              lambda_timeout: 10,
+              lambda_memory_size: 512,
+
+
+              asset: "[path].zip",
+              test: /\.html$|\.js$|\.xml$|\.rels$|/,
+              threshold: 10240,
+              minRatio: 0.8,
+              verbose: true
+            }),
+          ]
+        })
+      }
+    });
+  })
